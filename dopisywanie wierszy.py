afc8ea2315012e7c9e918e2e6a64ed0a099ef1b4
DevJa318 <52163558+DevJa318@users.noreply.github.com>
2020-07-23 23:34:29 +0200

    Add files via upload

---
+++ dopisywanie wierszy.py
@@ -0,0 +1,45 @@
+#! usr/bin/env python3
+#! encoding=utf-8
+
+import os
+
+folder = "www.dni.gov"
+Photon = "/home/devja3128/PROGRAMOWANIE/Photon"
+zapis = "/home/devja3128/study/webscriping/"
+
+#adres = '{}/{}/{}'.format(Photon,folder,nazwa_pliku)
+#print(adres)
+
+def sprawdzenie_folderu_zapisu(zapis, folder):
+# Sprawdza czy folder istnieje, jeśli nie tworzy go
+    lista = os.listdir(zapis)
+    if folder in lista:
+        pass #print("\nTen folder już istnieje\n")
+    else:
+        os.mkdir("{}/{}".format(zapis,folder))
+
+#print("Pliki w folderze", zapis, ":")
+#for item in lista:
+    #print("/p",item)
+sprawdzenie_folderu_zapisu(zapis, folder)
+#tekst, który ma zostać dopisany
+dodaj = "https://www.dni.gov/"
+nazwa_pliku = "files.txt"
+
+def dopisywanie(folder, nazwa_pliku, dodaj):
+# otwiera plik, pobiera linie, dopisuje tekst + linie do innego pliku
+
+    plik = open('/home/devja3128/Photon/{}/{}'.format(folder,nazwa_pliku), 'r+')
+    file = open('/home/devja3128/study/webscriping/{}/{}'.format(folder, nazwa_pliku), 'a+')
+
+
+    for line in plik:
+        if line.startswith('http'):
+            file.write(line)
+        else:
+            file.write(dodaj+line)
+
+    plik.close()
+    file.close()
+
+dopisywanie(folder, nazwa_pliku, dodaj)
